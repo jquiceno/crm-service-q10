21bcd3cb1fde33d203cc64a56c204acd60e4d6e0
Brayan Gamboa <bgamboa@q10.com>
2026-08-28 10:34:33 -0500

    fix: scope 500 response type addition to delete action only

--- src/Api/Controllers/AdsChannelsController.cs
+++ src/Api/Controllers/AdsChannelsController.cs
@@ -27,7 +27,6 @@ public sealed class AdsChannelsController(
     [ProducesResponseType(typeof(ApiSuccessResponse<CreateAdsChannelOutputDto>), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status409Conflict)]
-    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<HttpCreatedResult<CreateAdsChannelOutputDto>> CreateAdsChannel(
         [FromBody] CreateAdsChannelInputDto input,
         CancellationToken cancellationToken = default)
@@ -44,7 +43,6 @@ public sealed class AdsChannelsController(
     [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status409Conflict)]
-    [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status500InternalServerError)]
     public async Task<HttpOkResult<UpdateAdsChannelOutputDto>> UpdateAdsChannel(
         [FromRoute] int id,
         [FromBody] UpdateAdsChannelInputDto input,

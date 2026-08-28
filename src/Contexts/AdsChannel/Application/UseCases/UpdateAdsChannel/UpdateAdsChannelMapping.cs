af74147f6da0ed8d601b3b91b60eb443814e387a
Brayan Gamboa <bgamboa@q10.com>
2026-08-28 15:43:29 -0500

    refactor: unify Name nullability between Create and Update ads channel args

--- src/Contexts/AdsChannel/Application/UseCases/UpdateAdsChannel/UpdateAdsChannelMapping.cs
+++ src/Contexts/AdsChannel/Application/UseCases/UpdateAdsChannel/UpdateAdsChannelMapping.cs
@@ -5,7 +5,7 @@ namespace AdsChannel.Application.UseCases.UpdateAdsChannel;
 public static class UpdateAdsChannelMapping
 {
     public static UpdateAdsChannelArgs ToUpdateArgs(this UpdateAdsChannelInputDto input) =>
-        new(input.Name!, input.IsActive);
+        new(input.Name, input.IsActive);
 
     public static UpdateAdsChannelOutputDto ToOutputDto(this AdsChannelAggregate aggregate) =>
         new(aggregate.Id, aggregate.Name, aggregate.IsActive);
--- src/Contexts/AdsChannel/Domain/Aggregates/AdsChannelArgs.cs
+++ src/Contexts/AdsChannel/Domain/Aggregates/AdsChannelArgs.cs
@@ -2,4 +2,4 @@ namespace AdsChannel.Domain.Aggregates;
 
 public sealed record CreateAdsChannelArgs(string? Name, bool IsActive = true);
 
-public sealed record UpdateAdsChannelArgs(string Name, bool IsActive);
+public sealed record UpdateAdsChannelArgs(string? Name, bool IsActive);
